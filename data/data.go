4ccbc76435d43eef7143bcf96f4896d3775bf6a7
Karitham <plouis.pery@gmail.com>
2020-07-01 01:57:31 +0200

    messing around with mongo

--- data/data.go
+++ data/data.go
@@ -9,13 +9,30 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func database() {
+// UserBson represents how the data is stored in the database
+type UserBson struct {
+	UserID int64  `json:"UserID"`
+	Date   string `json:"Date"`
+	Waifus []int  `json:"Waifus"`
+}
+
+// InputStruct serves as an input scheme
+type InputStruct struct {
+	UserID int `json:"UserID"`
+	Waifu  int `json:"Waifu"`
+}
+
+var client *mongo.Client
+
+// InitDB is used to start the database
+func InitDB() {
 
 	// Set client options
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
 	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	var err error
+	client, err = mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,7 +44,22 @@ func database() {
 	}
 
 	fmt.Println("Connected to MongoDB!")
+}
 
-	users := client.Database("waifu").Collection("users")
-	fmt.Println(users)
+// Store handle the incoming data for the database
+func Store(input InputStruct) {
+	collection := client.Database("waifu").Collection("users")
+
+	insertResult, err := collection.InsertOne(context.TODO(), input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+}
+
+// ResetDB resets the database
+func ResetDB() {
+	client.Database("waifu").Drop(context.TODO())
+	fmt.Println("whole database emptied")
 }
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-import (
-	"bot/disc"
-	"os"
-)
+import "bot/data"
 
 func main() {
-	const filename = "/config.json"
-	dir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	path := dir + filename
-	disc.BotRun(path)
+	/* 	const filename = "/config.json"
+	   	dir, err := os.Getwd()
+	   	if err != nil {
+	   		panic(err)
+	   	}
+	   	path := dir + filename
+		   disc.BotRun(path) */
+
+	data.InitDB()
+	data.ResetDB()
 }
