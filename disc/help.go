4cc672db7d1e183246cdff2ccc898a310c69d5ee
Ismaël <30985701+LaneSh4d0w@users.noreply.github.com>
2020-07-15 02:22:33 +0200

    Update help.go

--- disc/help.go
+++ disc/help.go
@@ -10,13 +10,14 @@ func help(data *disgord.MessageCreate) {
 			Embed: &disgord.Embed{
 				Title: "Help",
 				Description: `
+				animeSearch (sa) : Search for a anime in the Anilist database 
 				roll (r) : Roll a new waifu
 				list (l) : List the waifus you have
 				search (s) : Search for a character by name / ID
 				profile (p) : Display profile information for yourself, or the user mentioned
 				favourite (f) : Set a favourite waifu to appear on your profile, you may choose any character you want
 				invite : Invite link to add the bot to your server
-				trendingAnime (ta) : Displays the top 10 trending animes from Anilist.
+				trendingAnimes (ta) : Displays the top 10 trending animes from Anilist
 				help (h) : Display this help page
 				`,
 				Color: 0xeec400,
