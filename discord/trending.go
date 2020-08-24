e6d4dcd32169023833990f596a34bed85783e86c
PL Pery <plouis.pery@gmail.com>
2020-08-25 00:58:14 +0200

    Update trending.go

--- discord/trending.go
+++ discord/trending.go
@@ -8,18 +8,22 @@ import (
 	"github.com/andersfylling/disgord"
 )
 
-func trendingAnime(data *disgord.MessageCreate, args []string) {
+func trendingMedia(data *disgord.MessageCreate, format string, args []string) {
 	var desc string
+	var formattedType string = "Anime "
+	if format != "ANIME" {
+		formattedType = "Manga "
+	}
 
 	// query the trending anime
-	res, err := query.TrendingSearch()
+	res, err := query.TrendingMediaQuery(format)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// return a formatted description
 	for i := range res.Page.Media {
-		desc += fmt.Sprintln(res.Page.Media[i].Title.Romaji)
+		desc += fmt.Sprintf("%d - %s\n", i+1, res.Page.Media[i].Title.Romaji)
 	}
 
 	// Send the message
@@ -28,18 +32,18 @@ func trendingAnime(data *disgord.MessageCreate, args []string) {
 		data.Message.ChannelID,
 		&disgord.CreateMessageParams{
 			Embed: &disgord.Embed{
-				Title:       "Animes Currently Trending",
+				Title:       formattedType + "Currently Trending",
 				Description: desc,
 				Color:       0x0e6b0e,
 				Footer: &disgord.EmbedFooter{
 					IconURL: "https://anilist.co/img/icons/favicon-32x32.png",
-					Text: "Trending anime list created from anilist",
+					Text:    "Trending " + formattedType + "list created from anilist",
 				},
 			},
 		},
 	)
 	if er != nil {
-		fmt.Println("There was an error sending trending anime message: ", er)
+		fmt.Println("There was an error sending trending media message: ", er)
 	}
 }
 
@@ -53,54 +57,18 @@ func trendingAnimeHelp(data *disgord.MessageCreate) {
 				Description: fmt.Sprintf(
 					"This is the help for the Trending Anime functionnality\n\n"+
 						"Trending anime is used to display the top 10 trending anime from anilist. Use it like so :\n"+
-						"`%strendingAnimes",
+						"`%strendingAnime",
 					conf.Prefix,
 				),
 				Footer: &disgord.EmbedFooter{
 					Text: fmt.Sprintf("Help requested by %s", data.Message.Author.Username),
 				},
-				Timestamp: data.Message.Timestamp,
-				Color:     0xeec400,
+				Color: 0xeec400,
 			},
 		},
 	)
 	if err != nil {
 		fmt.Println("There was an error sending trending anime help message: ", err)
-	}
-}
-
-func trendingManga(data *disgord.MessageCreate, args []string) {
-	var desc string
-
-	// query the trending manga
-	res, err := query.MangaTrendingSearch()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// return a formatted description
-	for i := range res.Page.Media {
-		desc += fmt.Sprintln(res.Page.Media[i].Title.Romaji)
-	}
-
-	// Send the message
-	_, er := client.CreateMessage(
-		ctx,
-		data.Message.ChannelID,
-		&disgord.CreateMessageParams{
-			Embed: &disgord.Embed{
-				Title:       "Mangas Currently Trending",
-				Description: desc,
-				Color:       0x0e6b0e,
-				Footer: &disgord.EmbedFooter{
-					IconURL: "https://anilist.co/img/icons/favicon-32x32.png",
-					Text: "Trending manga list created from anilist",
-				},
-			},
-		},
-	)
-	if er != nil {
-		fmt.Println("There was an error sending trending manga message: ", er)
 	}
 }
 
@@ -113,15 +81,14 @@ func trendingMangaHelp(data *disgord.MessageCreate) {
 				Title: "Trending Manga Help || alias tm",
 				Description: fmt.Sprintf(
 					"This is the help for the Trending Manga functionnality\n\n"+
-						"Trending manga is used to display the top 10 trending mangas from anilist. Use it like so :\n"+
+						"Trending manga is used to display the top 10 trending manga from anilist. Use it like so :\n"+
 						"`%strendingManga",
 					conf.Prefix,
 				),
 				Footer: &disgord.EmbedFooter{
 					Text: fmt.Sprintf("Help requested by %s", data.Message.Author.Username),
 				},
-				Timestamp: data.Message.Timestamp,
-				Color:     0xeec400,
+				Color: 0xeec400,
 			},
 		},
 	)
