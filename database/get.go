6cc21372f9845131129fa94b69ca7c5228e96986
Ismaël <30985701+LaneSh4d0w@users.noreply.github.com>
2020-09-29 08:20:59 +0200

    Add check functionality (fix)

--- database/get.go
+++ database/get.go
@@ -38,3 +38,28 @@ func ViewUserData(id disgord.Snowflake) (userData UserDataStruct) {
 	}
 	return
 }
+
+// CheckWaifuStruct is what data to send to check if a waifu is owned from another user in the database
+type CheckWaifuStruct struct {
+	UserID disgord.Snowflake `bson:"_id"`
+	CharID int
+}
+
+// CheckWaifuData is what data to send to check if a waifu is owned from another user in the database
+func (input CheckWaifuStruct) CheckWaifu() (WaifuExists bool) {
+	var decoded bson.M
+
+	// Find the character and delete it
+	var err = collection.FindOne(
+		context.TODO(),
+		bson.D{
+			primitive.E{Key: "_id", Value: input.UserID},
+			primitive.E{Key: "Waifus.ID", Value: input.CharID},
+		},
+	).Decode(&decoded) // If the database found something, returns true
+	if err != nil {
+		fmt.Println("There was an error when checking for a waifu :", err)
+		return false
+	}
+	return true
+}
