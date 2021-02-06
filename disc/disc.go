5a263d4615eb4d61baf69def163ebbcc7db2ee19
LBollenbach <33095440+loic.bollenbach@viacesi.fr>
2021-02-06 11:30:40 +0100

    Revert "Hotfix: Avoiding 0 division"
    This reverts commit 5b25d92e4c514f5c22ad711d26466f8b846cafcc.

--- disc/disc.go
+++ disc/disc.go
@@ -94,8 +94,9 @@ func Start(cf *config.ConfStruct) {
 
 			// Higher chances the more you interact with the bot
 			b.dropper.ChanInc[m.ChannelID]++
+			r := b.seed.Uint64() % (c.DropsOnInteract - b.dropper.ChanInc[m.ChannelID])
 
-			if b.seed.Uint64() == (c.DropsOnInteract - b.dropper.ChanInc[m.ChannelID]) || b.dropper.ChanInc[m.ChannelID]+1 == c.DropsOnInteract {
+			if r == 0 || b.dropper.ChanInc[m.ChannelID]+1 == c.DropsOnInteract {
 				b.drop(m)
 				b.dropper.ChanInc[m.ChannelID] = 0
 			}
