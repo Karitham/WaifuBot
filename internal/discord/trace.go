e8308fb8366ddba73c98efc55744952ad0a72b09
Karitham <plouis.pery@gmail.com>
2022-01-27 12:05:45 +0100

    fix tracing

--- internal/discord/trace.go
+++ internal/discord/trace.go
@@ -10,13 +10,13 @@ import (
 func trace(next corde.Handler) corde.Handler {
 	return func(w corde.ResponseWriter, i *corde.InteractionRequest) {
 		start := time.Now()
-		defer log.Trace().
+		next(w, i)
+
+		log.Trace().
 			Str("route", i.Data.Name).
 			Stringer("user", i.Member.User.ID).
 			Int("type", int(i.Type)).
 			Str("took", time.Since(start).String()).
 			Send()
-
-		next(w, i)
 	}
 }
